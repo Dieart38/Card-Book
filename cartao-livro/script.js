8a60fafcfddef5f9f8c62d7307f3689a8114f058
diego85 <149547878+Dieart38@users.noreply.github.com>
2024-10-13 11:25:37 -0300

    Delete cartao-livro directory

--- cartao-livro/script.js
+++
@@ -1,9 +0,0 @@
-document.querySelectorAll('.book').forEach(book => {
-    book.addEventListener('touchstart', () => {
-        book.classList.add('touch');
-    });
-
-    book.addEventListener('touchend', () => {
-        book.classList.remove('touch');
-    });
-});
